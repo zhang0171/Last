3ae1a7b0e54ecf34cd44d4f58042e7c3b57472f3
zhangFan <1124826087@qq.com>
2018-08-06 15:03:01 +0800

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import img1 from './img/Fan-tou-1.png';
+import img2 from './img/Fan-tou-2.png';
 import {BrowserRouter as Router,Route,Link,Redirect} from 'react-router-dom';
 import Sy from './components/sy'
 import Qyb from './components/qyb'
@@ -19,31 +20,39 @@ class App extends React.Component {
            <div className='Fan-bu'>
              <img className='Fan-bu-1' src={img1}/>
              <Router>
-             <div>
-                <div className='Fan-nav'>
-                <Link to='/sy'>首页</Link>
-                <Link to='/qyb'>企业版介绍</Link>
-                <Link to='/qj'>全屋定制</Link>
-                <Link to='/cj'>橱柜工具</Link>
-                <Link to='/ding'>吊顶工具</Link>
-                <Link to='/pz'>铺砖工具</Link>
-                <Link to='/dian'>到店购</Link>
-                <Link to='/sjal'>商家案例</Link>
+               <div className='Fan-nav'>
+                <div className='Fan-nav-1'>
+                  <Link to='/sy'>首页</Link>
+                  <Link to='/qyb'>企业版介绍</Link>
+                  <Link to='/qj'>全屋定制</Link>
+                  <Link to='/cj'>橱柜工具</Link>
+                  <Link to='/ding'>吊顶工具</Link>
+                  <Link to='/pz'>铺砖工具</Link>
+                  <Link to='/dian'>到店购</Link>
+                  <Link to='/sjal'>商家案例</Link>
                 </div>
-                <Route path='/sy' component={Sy}></Route>
-                <Route path='/qyb' component={Qyb}></Route>
-                <Route path='/qj' component={Qj}></Route>
-                <Route path='/cj' component={Cj}></Route>
-                <Route path='/ding' component={Ding}></Route>
-                <Route path='/pz' component={Pz}></Route>
-                <Route path='/dian' component={Dian}></Route>
-                <Route path='/sjal' component={Sjal}></Route>
+               
+                <div className='Fan-nav-2'>
+                  <Route path='/sy' component={Sy}></Route>
+                  <Route path='/qyb' component={Qyb}></Route>
+                  <Route path='/qj' component={Qj}></Route>
+                  <Route path='/cj' component={Cj}></Route>
+                  <Route path='/ding' component={Ding}></Route>
+                  <Route path='/pz' component={Pz}></Route>
+                  <Route path='/dian' component={Dian}></Route>
+                  <Route path='/sjal' component={Sjal}></Route>
+                </div>  
+
+
+
               </div>
-              
              </Router>
+                <img className='Fan-bu-2' src={img2}/>
+                <span className='Fan-zi'>
+                  0571-28070168
+                </span>
               
-             
-             <img />
+              
            </div>
         </div>
         
