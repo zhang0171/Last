d84c8f798590e3f63f0e22357534f2b93504a07a
zhangFan <1124826087@qq.com>
2018-08-06 17:12:31 +0800

    5

--- src/App.js
+++ src/App.js
@@ -13,49 +13,57 @@ import Sjal from './components/sjal';
 
 
 class App extends React.Component {
+  one(){
+    window.location.reload();
+  }
+
   render() {
     return (
       <div>
         <div className="Fan-tou">
            <div className='Fan-bu'>
-             <img className='Fan-bu-1' src={img1}/>
+             
              <Router>
                <div className='Fan-nav'>
                 <div className='Fan-nav-1'>
-                  <Link to='/sy'>首页</Link>
-                  <Link to='/qyb'>企业版介绍</Link>
-                  <Link to='/qj'>全屋定制</Link>
-                  <Link to='/cj'>橱柜工具</Link>
-                  <Link to='/ding'>吊顶工具</Link>
-                  <Link to='/pz'>铺砖工具</Link>
-                  <Link to='/dian'>到店购</Link>
-                  <Link to='/sjal'>商家案例</Link>
+                  <img className='Fan-bu-1' src={img1}/>
+                  <Link onClick={this.one} to='/sy'>首页</Link>
+                  <Link onClick={this.one} to='/qyb'>企业版介绍</Link>
+                  <Link onClick={this.one} to='/qj'>全屋定制</Link>
+                  <Link onClick={this.one} to='/cj'>橱柜工具</Link>
+                  <Link onClick={this.one} to='/ding'>吊顶工具</Link>
+                  <Link onClick={this.one} to='/pz'>铺砖工具</Link>
+                  <Link onClick={this.one} to='/dian'>到店购</Link>
+                  <Link onClick={this.one} to='/sjal'>商家案例</Link>
+                  <img className='Fan-bu-2' src={img2}/>
+                  <span className='Fan-zi'>
+                    0571-28070168
+                  </span>
                 </div>
                
-                <div className='Fan-nav-2'>
-                  <Route path='/sy' component={Sy}></Route>
-                  <Route path='/qyb' component={Qyb}></Route>
-                  <Route path='/qj' component={Qj}></Route>
-                  <Route path='/cj' component={Cj}></Route>
-                  <Route path='/ding' component={Ding}></Route>
-                  <Route path='/pz' component={Pz}></Route>
-                  <Route path='/dian' component={Dian}></Route>
-                  <Route path='/sjal' component={Sjal}></Route>
-                </div>  
-
-
-
               </div>
-             </Router>
-                <img className='Fan-bu-2' src={img2}/>
-                <span className='Fan-zi'>
-                  0571-28070168
-                </span>
-              <b>啊啊啊啊啊啊啊啊啊啊啊啊啊啊</b>
-              <i>我我我我我哦我我我哦我我我我哦我我vggff</i>
+             </Router> 
            </div>
         </div>
         
+
+        <session>
+          <Router>
+          <div className='Fan-666'>
+              <Route path='/sy' component={Sy}></Route>
+              <Route path='/qyb' component={Qyb}></Route>
+              <Route path='/qj' component={Qj}></Route>
+              <Route path='/cj' component={Cj}></Route>
+              <Route path='/ding' component={Ding}></Route>
+              <Route path='/pz' component={Pz}></Route>
+              <Route path='/dian' component={Dian}></Route>
+              <Route path='/sjal' component={Sjal}></Route>
+          </div>  
+
+          </Router>
+
+        </session>
+
         {/* <div>尾部</div> */}
       </div>
     );
