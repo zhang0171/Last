92953659262e1252cefc09da2712b89a76f985a5
zhangFan <1124826087@qq.com>
2018-08-06 18:01:38 +0800

    11

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ import Pz from './components/pz';
 import Dian from './components/dian';
 import Sjal from './components/sjal';
 
-
+const active={color:'red'}
 class App extends React.Component {
   one(){
-    window.location.reload();
+    // window.location.reload();
   }
 
   render() {
@@ -27,14 +27,16 @@ class App extends React.Component {
                <div className='Fan-nav'>
                 <div className='Fan-nav-1'>
                   <img className='Fan-bu-1' src={img1}/>
-                  <Link onClick={this.one} to='/sy'>首页</Link>
-                  <Link onClick={this.one} to='/qyb'>企业版介绍</Link>
-                  <Link onClick={this.one} to='/qj'>全屋定制</Link>
-                  <Link onClick={this.one} to='/cj'>橱柜工具</Link>
-                  <Link onClick={this.one} to='/ding'>吊顶工具</Link>
-                  <Link onClick={this.one} to='/pz'>铺砖工具</Link>
-                  <Link onClick={this.one} to='/dian'>到店购</Link>
-                  <Link onClick={this.one} to='/sjal'>商家案例</Link>
+                  <div className='Fan-link'>
+                  <Link className='Fan-zi-1' onClick={this.one} to='/sy' activeStyle={active}>首页</Link>
+                  <Link className='Fan-zi-2' onClick={this.one} to='/qyb'>企业版介绍</Link>
+                  <Link className='Fan-zi-3' onClick={this.one} to='/qj'>全屋定制</Link>
+                  <Link className='Fan-zi-4' onClick={this.one} to='/cj'>橱柜工具</Link>
+                  <Link className='Fan-zi-5' onClick={this.one} to='/ding'>吊顶工具</Link>
+                  <Link className='Fan-zi-6' onClick={this.one} to='/pz'>铺砖工具</Link>
+                  <Link className='Fan-zi-7' onClick={this.one} to='/dian'>到店购</Link>
+                  <Link className='Fan-zi-8' onClick={this.one} to='/sjal'>商家案例</Link>
+                  </div>
                   <img className='Fan-bu-2' src={img2}/>
                   <span className='Fan-zi'>
                     0571-28070168
